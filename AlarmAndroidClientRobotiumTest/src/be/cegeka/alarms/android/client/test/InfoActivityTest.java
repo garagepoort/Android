1ef9798413560675dfe1456b0bdac3a9c0457817
garagepoort <david.s.maes@gmail.com>
2013-04-26 09:30:31 +0200

    dz

---
+++ AlarmAndroidClientRobotiumTest/src/be/cegeka/alarms/android/client/test/InfoActivityTest.java
@@ -0,0 +1,54 @@
+package be.cegeka.alarms.android.client.test;
+
+import android.app.Activity;
+import android.test.ActivityInstrumentationTestCase2;
+import be.cegeka.alarms.android.client.activities.InfoActivity;
+import be.cegeka.alarms.android.client.activities.LoginActivity;
+import be.cegeka.alarms.android.client.activities.SavedAlarmsActivity;
+
+import com.jayway.android.robotium.solo.Solo;
+
+
+public class InfoActivityTest extends ActivityInstrumentationTestCase2<InfoActivity> {
+
+	private Solo solo; 
+//	private InternetChecker internetChecker;
+	
+	public InfoActivityTest() {
+		super(InfoActivity.class);
+	}
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		solo = new Solo(getInstrumentation(), getActivity());
+//		internetChecker = EasyMock.createMock(InternetChecker.class);
+//		getActivity().setInternetChecker(internetChecker);
+	}
+
+	public void testCheckNoInternetDialog() {
+//		EasyMock.expect(internetChecker.isNetworkAvailable(getActivity())).andReturn(false);
+		
+		solo.clickOnButton("Log In");
+		solo.waitForDialogToOpen(2000);
+		assertTrue(solo.searchText("No internet connection. Try again later"));
+		solo.clickOnButton("Ok");
+	}
+
+	public void testCheckLoginActivity() {
+//		EasyMock.expect(internetChecker.isNetworkAvailable(getActivity())).andReturn(true);
+		
+		solo.clickOnButton("Log In");
+		solo.waitForActivity(LoginActivity.class, 2000);
+		solo.enterText(0, "david.s.maes@gmail.com");
+		solo.enterText(1, "password");
+		solo.clickOnButton("Sign in or register");
+		solo.waitForActivity(Activity.class);
+		solo.assertCurrentActivity("", InfoActivity.class);
+	}
+
+	public void testShowAlarms() {
+		solo.clickOnButton("Show Alarms");
+		solo.waitForActivity(SavedAlarmsActivity.class);
+		assertTrue(solo.searchText("Saved Alarms"));
+	}
+}
