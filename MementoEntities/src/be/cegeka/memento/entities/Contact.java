93e8c0c097928696b4f83b7027781afd66da3d5d
garagepoort <david.s.maes@gmail.com>
2013-05-30 12:57:45 +0200

    entities update

--- MementoEntities/src/be/cegeka/memento/entities/Contact.java
+++ MementoEntities/src/be/cegeka/memento/entities/Contact.java
@@ -4,21 +4,19 @@ import java.io.Serializable;
 
 import be.cegeka.memento.exceptions.ContactException;
 
-public class Contact implements Serializable {
-	
+public class Contact implements Comparable, Serializable{
+
 	private static final long serialVersionUID = 1L;
 	private long id;
 	private String naam;
-	private String voornaam;
 	private String email;
 	private String tel;
 
 	public Contact() {
 	}
 
-	public Contact(String naam, String voornaam, String email, String tel) throws ContactException {
+	public Contact(String naam, String email, String tel) throws ContactException {
 		setNaam(naam);
-		setVoornaam(voornaam);
 		setEmail(email);
 		setTel(tel);
 	}
@@ -28,27 +26,23 @@ public class Contact implements Serializable {
 	}
 
 	public void setNaam(String naam) throws ContactException {
-		if (naam == null || naam.isEmpty())
-			throw new ContactException("naam is null or empty");
+				if (naam == null || naam.isEmpty())
+					throw new ContactException("naam is null or empty");
 
 		this.naam = naam;
 	}
 
-	public void setVoornaam(String voornaam) throws ContactException {
-		if (voornaam == null || voornaam.isEmpty())
-			throw new ContactException("voornaam is null or empty");
-
-		this.voornaam = voornaam;
-	}
-
 	public void setEmail(String email) throws ContactException {
-		if (email == null || email.isEmpty())
-			throw new ContactException("email is null or empty");
+		if (email == null || email.isEmpty()) {
+			this.email = "";
 
-		if (!email.contains("@"))
+		}
+		else if (!email.contains("@")) {
 			throw new ContactException("email has invalid structure");
-
-		this.email = email;
+		}
+		else {
+			this.email = email;
+		}
 	}
 
 	public void setTel(String tel) throws ContactException {
@@ -66,10 +60,6 @@ public class Contact implements Serializable {
 		return naam;
 	}
 
-	public String getVoornaam() {
-		return voornaam;
-	}
-
 	public String getEmail() {
 		return email;
 	}
@@ -77,4 +67,59 @@ public class Contact implements Serializable {
 	public String getTel() {
 		return tel;
 	}
+
+	@Override
+	public String toString() {
+		return naam + " " + " (" + id + ")";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((naam == null) ? 0 : naam.hashCode());
+		result = prime * result + ((tel == null) ? 0 : tel.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contact other = (Contact) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id != other.id)
+			return false;
+		if (naam == null) {
+			if (other.naam != null)
+				return false;
+		} else if (!naam.equals(other.naam))
+			return false;
+		if (tel == null) {
+			if (other.tel != null)
+				return false;
+		} else if (!tel.equals(other.tel))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int compareTo(Object other) {
+		if(other instanceof Contact){
+			return this.toString().compareTo(other.toString());
+		}
+		return -1;
+	}
+
+
 }
